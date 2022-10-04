94402da8b32cd0df4c776bd4340da2f294b8b113
EC2 Default User <ec2-user@ip-172-31-62-9.ec2.internal>
2022-10-04 15:48:41 +0000

    final

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ziggy04/ziggy04:1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment ziggy04-app --image=ziggy04/ziggy04:1.0
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/ziggy04-app --address 0.0.0.0 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,7 +5,7 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ziggy04/ziggy04:1.0
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +13,4 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
